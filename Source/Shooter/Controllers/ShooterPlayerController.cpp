c3b2c29c8ab8b4a53a0581372fb59b3d760a98c7
gurgenkarapetyan <gurgen.karapetyan.noor@gmail.com>
2022-02-22 16:27:39 +0400

    - added custom player controller - added ammo widget on main screen

---
+++ Source/Shooter/Controllers/ShooterPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+AShooterPlayerController::AShooterPlayerController()
+{
+	
+}
+
+void AShooterPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HUDOverlayWidget)
+	{
+		HUDOverlay = CreateWidget<UUserWidget>(this, HUDOverlayWidget);
+
+		if (HUDOverlay)
+		{
+			HUDOverlay->AddToViewport();
+			HUDOverlay->SetVisibility(ESlateVisibility::Visible);
+		}
+	}
+}
---
+++ Source/Shooter/Controllers/ShooterPlayerController.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AShooterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AShooterPlayerController();
+
+	virtual void BeginPlay() override;
+private:
+	/** Reference to the Overall HUD Overlay Blueprint Class. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> HUDOverlayWidget;
+
+	/** Variable to hold the HUD Overlay Widget after creating it. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Widgets", meta = (AllowPrivateAccess = "true"))
+	UUserWidget* HUDOverlay;
+};
