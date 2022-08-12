6a7625e1c9f490b4f996e855bd4e9431ab8bee68
gurgenkarapetyan <gurgen.karapetyan.noor@gmail.com>
2022-08-12 15:04:37 +0400

    - added physics

--- Source/Shooter/Shooter.h
+++ Source/Shooter/Shooter.h
@@ -4,3 +4,8 @@
 
 #include "CoreMinimal.h"
 
+#define EPS_METAL EPhysicalSurface::SurfaceType1
+#define EPS_STONE EPhysicalSurface::SurfaceType2
+#define EPS_TILE EPhysicalSurface::SurfaceType3
+#define EPS_GRASS EPhysicalSurface::SurfaceType4
+#define EPS_WATER EPhysicalSurface::SurfaceType5
